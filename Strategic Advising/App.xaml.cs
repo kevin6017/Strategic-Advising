11b4334dfa4154bb670b2a9febb5cf56522e09e0
Kevin <kmcminn@bsu.edu>
2017-10-13 09:52:14 -0400

    Add project files.

---
+++ Strategic Advising/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Strategic_Advising
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
