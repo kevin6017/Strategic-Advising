77628b462271d0fce5c1aa8ca13ae32a7254fa40
Kevin <kmcminn@bsu.edu>
2017-10-16 11:11:10 -0400

    added Datagrid test page

---
+++ Strategic Advising/DataGridTest.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Strategic_Advising
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        DataTable dataTable;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataTable = new DataTable("sampleTable");
+            DataColumn dc1 = new DataColumn("CourseID", typeof(string));
+            dataTable.Columns.Add(dc1);
+            sampleGrid.ItemsSource = dataTable.DefaultView;
+        }
+        
+    }
+}
