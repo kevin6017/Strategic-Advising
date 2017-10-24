c404f2dd223b034f5bd8b46c0dc275b399a4ec72
Brian Pierson <bcpierson@bsu.edu>
2017-10-24 19:47:45 -0400

    Adds test semester json file and semester class

---
+++ Strategic Advising/Semester.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategic_Advising
+{
+    class Semester
+    {
+        public String semester { get; set; }
+
+        public int position { get; set; }
+
+        public string[] classes { get; set; }
+    }
+}
