5e257d405becdc0a324bbf42fd288905c14a9a54
Kevin <kmcminn@bsu.edu>
2017-10-31 19:32:42 -0400

    adds checkbox column to completed classes

--- Strategic Advising/DataGridTest.xaml.cs
+++ Strategic Advising/DataGridTest.xaml.cs
@@ -37,18 +37,24 @@ namespace Strategic_Advising
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             dataTable = new DataTable("sampleTable");
+            
             DataColumn dc1 = new DataColumn("Course Number", typeof(string));
             DataColumn dc2 = new DataColumn("Course Title", typeof(string));
             DataColumn dc3 = new DataColumn("Credit Hours", typeof(int));
             DataColumn dc4 = new DataColumn("Spring Class", typeof(bool));
             DataColumn dc5 = new DataColumn("Fall Class", typeof(bool));
             DataColumn dc6 = new DataColumn("Prerequisites", typeof(string));
+
+            dc4.ReadOnly = true;
+            dc5.ReadOnly = true;
+
             dataTable.Columns.Add(dc1);
             dataTable.Columns.Add(dc2);
             dataTable.Columns.Add(dc3);
             dataTable.Columns.Add(dc4);
             dataTable.Columns.Add(dc5);
             dataTable.Columns.Add(dc6);
+            
             sampleGrid.ItemsSource = dataTable.DefaultView;
             var JSONclasses = loadCourseList();
             for (var i=0; i<JSONclasses.Count; i++) //theres an extra row being created here? (Issue #2)
@@ -60,9 +66,16 @@ namespace Strategic_Advising
                 dr[3] = JSONclasses[i].fall; //these two are clickable (Issue #1)
                 dr[4] = JSONclasses[i].spring; //not sure how to lock them
                 dr[5] = string.Join(", ", JSONclasses[i].prerequisites);
+                
                 dataTable.Rows.Add(dr);
                 sampleGrid.ItemsSource = dataTable.DefaultView;
             }
+            DataGridCheckBoxColumn checkColumn = new DataGridCheckBoxColumn();
+            checkColumn.Header = "Class taken?";
+            checkColumn.Width = 100;
+            sampleGrid.IsReadOnly = false;
+            sampleGrid.Columns.Add(checkColumn);
+
         }
 
         private List<Course> loadCourseList()
