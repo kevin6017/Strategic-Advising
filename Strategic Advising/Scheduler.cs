c75da610fe9a693fe175c6e1d961077d3f11e0fe
Brian Pierson <bcpierson@bsu.edu>
2018-04-11 18:20:00 -0400

    Makes HONR 499 always be scheduled in the last year of schooling

--- Strategic Advising/Scheduler.cs
+++ Strategic Advising/Scheduler.cs
@@ -285,7 +285,7 @@ namespace Strategic_Advising
                    
                 }
             }
-            if (currentCourse.courseNumber == "CS495" || currentCourse.courseNumber == "CS498" || currentCourse.courseNumber == "MATH498")
+            if (currentCourse.courseNumber == "CS495" || currentCourse.courseNumber == "CS498" || currentCourse.courseNumber == "MATH498" || currentCourse.courseNumber == "HONR499")
             {
                 int remainingCredits = -currentCourse.creditHours;
                 foreach (Course c in remainingCourseList)
